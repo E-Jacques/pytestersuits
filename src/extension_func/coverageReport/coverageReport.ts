bd7a5d0b627ae321b23249284846d2bb71dee9ee
Emilien <emil00j77@gmail.com>
2021-12-23 10:47:09 +0100

    Remove icon on coverage report when it's file report

--- src/extension_func/coverageReport/coverageReport.ts
+++ src/extension_func/coverageReport/coverageReport.ts
@@ -6,19 +6,24 @@ export class CoverageReport extends vscode.TreeItem {
    * 
    * @param state null when that coverage report for a file
    */
-    constructor(
-        public readonly filepath: string,
-        private line: number,
-        private state: ("NH" | "NT") | null,
-        public readonly collapsibleState: vscode.TreeItemCollapsibleState
-      ) {
-        super(filepath, collapsibleState);
-        this.tooltip = `Go to ${this.filepath}:${this.line}`;
-        this.description = this.state === null ? `${this.line}%` : `l:${this.line}`;
-        this.iconPath = join(__dirname, "rsc", "icons", `coverage_report_${this.state}.svg`);
-    }
+  constructor(
+    public readonly filepath: string,
+    private line: number,
+    private state: ("NH" | "NT") | null,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState
+  ) {
+    super(filepath, collapsibleState);
+    this.tooltip = `Go to ${this.filepath}:${this.line}`;
+    this.description = this.state === null ? `${this.line}%` : `l:${this.line}`;
+    let imagePath = join(__filename, "..", "..", "..", "rsc", "icons", `coverage_report_${this.state}.svg`);
+    this.iconPath = { light: imagePath, dark: imagePath };
 
-    public isFileReport () {
-      return this.state === null;
+    if (this.state === null) {
+      this.iconPath = undefined;
     }
+  }
+
+  public isFileReport() {
+    return this.state === null;
+  }
 }

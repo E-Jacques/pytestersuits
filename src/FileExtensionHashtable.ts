cb7d22e48523d05c573a63272b084a5b783ef278
Emilien <emil00j77@gmail.com>
2022-01-07 15:53:12 +0100

    Added 'js' to FileExtension hashtable

--- src/FileExtensionHashtable.ts
+++ src/FileExtensionHashtable.ts
@@ -1,5 +1,6 @@
 import { LanguageInterface } from "./extension_func/languageInterface";
 import * as PythonHandler from "./extension_func/language/PythonHandler";
+import * as JavascriptHandler from "./extension_func/language/JavascriptHandler";
 
 interface HashTable {
     [key: string]: any;
@@ -7,6 +8,7 @@ interface HashTable {
 
 let hashTable: HashTable = {};
 hashTable["py"] = PythonHandler.PythonHandler;
+hashTable["js"] = JavascriptHandler.JavascriptHandler;
 
 export function getDefaultLanguage (): LanguageInterface {
     return hashTable[Object.keys(hashTable)[0]];

84f2075e9d142283f0f2c7facadfef7c66056bf2
Emilien <emil00j77@gmail.com>
2022-01-13 10:08:49 +0100

    Add test to get configuration for default libraries

---
+++ src/test/suite/python/config.test.ts
@@ -0,0 +1,27 @@
+import * as vscode from "vscode";
+import * as assert from "assert";
+import PythonHandler from "../../../extension_func/language/python/PythonHandler";
+import { after } from "mocha";
+
+suite("Testing python's default library configuration", () => {
+    after(async () => {
+        const extensionConfig = vscode.workspace.getConfiguration("pytestersuits");
+        let defaultValue = extensionConfig.inspect<string>("pythonDefaultTestLibrary")?.defaultValue;
+        await extensionConfig.update("pythonDefaultTestLibrary", defaultValue, true);
+    });
+
+    test("Should return null by default", () => {
+        const python = new PythonHandler();
+        let lib = python.getConfigLibrary();
+        assert.strictEqual(lib, null);
+    });
+
+    test("Should return pytest", async () => {
+        let extensionConfig = vscode.workspace.getConfiguration("pytestersuits");
+        await extensionConfig.update("pythonDefaultTestLibrary", "pytest", true);
+
+        extensionConfig = vscode.workspace.getConfiguration("pytestersuits");
+        let lib = new PythonHandler().getConfigLibrary();
+        assert.strictEqual(lib?.name, "pytest");
+    });
+});
---
+++ src/test/suite/typescript/config.test.ts
@@ -0,0 +1,27 @@
+import * as vscode from "vscode";
+import * as assert from "assert";
+import TypescriptHandler from "../../../extension_func/language/typescript/TypescriptHandler";
+import { after } from "mocha";
+
+suite ("Testing python's default library configuration", () => {
+    after(async () => {
+        const extensionConfig = vscode.workspace.getConfiguration("pytestersuits");
+        let defaultValue = extensionConfig.inspect<string>("typescriptDefaultTestLibrary")?.defaultValue;
+        await extensionConfig.update("typescriptDefaultTestLibrary", defaultValue, true);
+    });
+
+    test("Should return null by default", () => {
+        let python = new TypescriptHandler();
+        let lib = python.getConfigLibrary();
+        assert.strictEqual(lib, null);
+    });
+
+    test("Should return pytest", async () => {
+        let extensionConfig = vscode.workspace.getConfiguration("pytestersuits");
+        await extensionConfig.update("typescriptDefaultTestLibrary", "Vscode Extension Test suit", true);
+
+        extensionConfig = vscode.workspace.getConfiguration("pytestersuits");
+        let lib = new TypescriptHandler().getConfigLibrary();
+        assert.strictEqual(lib?.name, "Vscode Extension Test suit");
+    });
+});
